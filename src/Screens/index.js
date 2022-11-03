8f3ced2143edd80a9cfefa28d7e553f9030624db
Victor Ezea <victorvikson@gmail.com>
2022-11-03 17:45:46 +0100

    Initian commit && project setUp

---
+++ src/Screens/index.js
@@ -0,0 +1,15 @@
+import LoginScreen from './LoginScreen';
+import HomeScreen from './HomeScreen';
+import NewsDetailsScreen from './NewsDetailsScreen';
+import SignUpScreen from './SignUpScreen';
+import SplashScreen from './SplashScreen';
+import UserScreen from './UserScreen';
+
+export {
+  LoginScreen,
+  HomeScreen,
+  NewsDetailsScreen,
+  SignUpScreen,
+  SplashScreen,
+  UserScreen,
+};
